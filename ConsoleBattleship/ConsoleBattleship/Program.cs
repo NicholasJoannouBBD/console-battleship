2bbc47a568419db4aba8de4b5fc2fdf5171e3ef9
Adam Kaminski <arturkars01@gmail.com>
2023-04-12 18:01:53 +0200

    Basic .NET 6 Console application skeleton code, auto-generated

---
+++ ConsoleBattleship/ConsoleBattleship/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// The templates have changed, in main, we no longer see the top level statements normally used, this can be a pain, but its part
+// of .NET 6 and I thought it best to go with the latest LTS support
