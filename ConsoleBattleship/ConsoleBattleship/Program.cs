dd36d03b9b4284e2135155ee2ed1a27aaf8396be
Nicholas Joannou <nicholasj@bbd.co.za>
2023-04-15 11:42:37 +0200

    Added DbScript file, Added db file and added DbHandler file with created db functions

--- ConsoleBattleship/ConsoleBattleship/Program.cs
+++ ConsoleBattleship/ConsoleBattleship/Program.cs
@@ -1,4 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// The templates have changed, in main, we no longer see the top level statements normally used, this can be a pain, but its part
-// of .NET 6 and I thought it best to go with the latest LTS support
+﻿using ConsoleBattleship;
+using System.Data.SQLite;
+
+DbHandler dbHandler = new DbHandler();
+
+string url = @"URI=file:battleship.db";
+using var connection = new SQLiteConnection(url);
+connection.Open();
