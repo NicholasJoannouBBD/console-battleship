2f6a8ea846a31801f79b5d89b00a94216adeaaad
Xander Lubbe <x.lubbe@icloud.com>
2023-04-20 07:58:30 +0200

    Minor updates

--- ConsoleBattleship/ConsoleBattleship/Scores/score.cs
+++ ConsoleBattleship/ConsoleBattleship/Scores/score.cs
@@ -9,37 +9,60 @@ namespace ConsoleBattleship.Score
     public class Score {
     private int player1Score;
     private int player2Score;
+    public bool isGameActive;
     
     public Score()
     {
         player1Score = 0;
         player2Score = 0;
+        // isGameActive here
     }
     
     public void IncrementPlayer1Score(int points)
     {
         player1Score =+ points;
+        if(this.detectWin(player1Score) == true){
+            // updateLeaderboard();
+        }
     }
     
     public void IncrementPlayer2Score(int points)
     {
         player2Score =+ points;
+        this.detectWin(player2Score);
+
     }
     
-    public int GetPlayer1Score()
+    public int getPlayer1Score()
     {
         return player1Score;
     }
     
-    public int GetPlayer2Score()
+    public int getPlayer2Score()
     {
         return player2Score;
     }
     
-    public void ResetScores()
+    public void resetGame()
     {
         player1Score = 0;
         player2Score = 0;
+        // game should be actuve when played, and be not when it ends or no game ic played. 
+        // This means isGameActive would need to be updated extenally when a new game is being set up.
+        isGameActive = true;
+    }
+
+    public bool detectWin(int score){
+        if (score >= 17){
+            // need to check after every sunk ship is this is false, if so the game ends.
+            isGameActive = false;
+            return true;
+        } 
+        return false;
+    }
+
+    public void updateLeaderboard(string winningPlayer, string loosingPlayer){
+        // update db with one win, one loss
     }
 }
 }
